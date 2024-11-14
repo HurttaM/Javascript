549a6cb61dc88d9e729506b029366beec41ad2b0
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-11-14 09:39:07 +0200

    Moduuli 2

--- Moduuli 2/js/teht-1.js
+++ Moduuli 2/js/teht-1.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let numbers = [];
+
+function number(times) {
+    for (let i = 0; i < times; i++) {
+        const num = prompt('Give a number')
+        numbers.push(num)
+    }
+}
+
+number(5)
+
+for (let i = numbers.length - 1; i >= 0; i--) {
+    console.log(numbers[i]);
+}
+
