8476ff9726d45a3a0765374d279aa8c52f730ec1
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-11-14 09:52:15 +0200

    Moduuli 2

--- Moduuli 2/js/teht-2.js
+++ Moduuli 2/js/teht-2.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let participants = [];
+
+function participant(amount) {
+    for (let i = 0; i < amount; i++) {
+        const name = prompt('Give a name of a participant');
+        participants.push(name);
+    }
+}
+
+
+const number = parseInt(prompt('How many participants?'), 10);
+
+participant(number);
+
+participants.sort();
+
+let List = "<ol>";
+for (let i = 0; i < participants.length; i++) {
+    List += `<li>${participants[i]}</li>`;
+}
+List += "</ol>";
+
+document.getElementById("results").innerHTML = List;
